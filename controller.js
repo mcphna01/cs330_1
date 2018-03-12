a76e6a774e2cb47193ccec88ab86a7e5c6a51561
Nathan James McPhetres <mcphna01@luther.edu>
2018-03-11 22:58:25 -0500

    modified controller

--- controller.js
+++ controller.js
@@ -4,11 +4,11 @@ var shoppingModel= new ShoppingList()
 var myView=new ShoppingView(shoppingModel)
 
 function clickedon() {
-	let rowcolids=['item', 'quantity', 'store', 'section', 'price', 'priority']
+	let rowcolids=['item', 'quantity', 'priority', 'store', 'section', 'price']
 	let vals={}
 	for (let cid of rowcolids) {
 		vals[cid]=document.getElementById(cid).value
 	}
-	let it=new Item(vals.item, vals.quantity, vals.store, vals.section, vals.price, vals.priority)
+	let it=new Item(vals.item, vals.quantity, vals.priority, vals.store, vals.section, vals.price)
 	shoppingModel.addItem(it)
 }
