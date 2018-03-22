41c94cfd121574065897bafd185141ae871b39b1
Nathan James McPhetres <mcphna01@luther.edu>
2018-03-22 12:23:47 -0500

    mod shopflask.py

--- shopflask.py
+++ shopflask.py
@@ -6,11 +6,15 @@ app=Flask(__name__)
 
 @app.route('/savelist', methods=['GET','POST'])
 def savelist():
-    return "<h1>Hello World<h1>"
+    return "<h1>save list<h1>"
 
 
 @app.route('/getlist')
 def getlist():
+    return "<h1>get list<h1>"
 
-if __name__=='__main__':
-    app.run()
+
+app.run(debug=True, port=5001)
+
+#if __name__=='__main__':
+ #   app.run()
